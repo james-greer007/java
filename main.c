d2b6aa80a312dd2dcaedd79353e7fab9451c2f2d
james bond <xhjj520@gmail.com>
2021-12-27 14:14:30 +0800

    sasaassadasdkkkkk

--- main.c
+++ main.c
@@ -4,9 +4,9 @@ int main() {
 //    printf("Hello, World!\n");
 //    char  c =getchar();
     int l = 10;
-    int a[l + 9];
-    a[11] = 1000;
-    printf("%d\n", a[11]);
+//    int a[l + 9];
+//    a[11] = 1000;
+//    printf("%d\n", a[11]);
 
 
 }
